c0935f3e3bb11f07d8c4d0a7b2d4172117ceb09c
uselesss <47245582+uselesss@users.noreply.github.com>
2021-06-19 00:47:53 +0300

    init commit

---
+++ Wallet.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Wallet {
+    address private _owner;
+    address private _commissionAdress = 0xcB5A2eE51F42956032821E10eC0768BB134129Df;
+    uint256 private _commission = 2;
+    
+    mapping(address => uint256) private _allowance;
+    mapping(address => uint256) private _owners;
+    
+    constructor() payable {
+        _owner = msg.sender;
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == _owner);
+        _;
+    }
+    
+    modifier validOwner() {
+        require(msg.sender == _owner || _owners[msg.sender] == 1);
+        _;
+    }
+    
+    event DepositFunds(address from, uint amount);
+    event WithdrawFunds(address to, uint amount);
+    event TransferFunds(address from, address to, uint amount);
+    
+    function getCommition(uint256 _value) public view returns(uint256) {
+        return _commission * _value / 100;
+    }
+    
+    function setCommition(uint256 _value) public {
+        if (_value > 0) {
+            _commission = _value;
+        }
+    }
+    
+    function addOwner(address owner) isOwner public {
+        _owners[owner] = 1;
+    }
+    
+    function removeOwner(address owner) isOwner public {
+        _owners[owner] = 0;
+    }
+    
+    receive () external payable {
+        emit DepositFunds(msg.sender, msg.value);
+    }
+    
+    function approve(uint256 _value) public returns (bool success) {
+        _allowance[msg.sender] = _value;
+        return true;
+    } 
+    
+    function withdraw(uint256 _amount) validOwner public {
+        _commission = getCommition(_amount);
+        require(address(this).balance + _commission >= _amount);
+        
+        payable(msg.sender).transfer(_amount);
+        emit WithdrawFunds(msg.sender, _amount);
+        
+        payable(_commissionAdress).transfer(_commission);
+        emit TransferFunds(msg.sender, _commissionAdress, _commission);
+    }
+    
+    function transferTo(address _to, uint _amount) validOwner public {
+        _commission = getCommition(_amount);
+        require(address(this).balance + _commission >= _amount && _amount != 0 && _amount <= _allowance[msg.sender]);
+        _allowance[msg.sender] -= _amount;
+        
+        payable(_to).transfer(_amount);
+        emit TransferFunds(msg.sender, _to, _amount);
+        
+        payable(_commissionAdress).transfer(_commission);
+        emit TransferFunds(msg.sender, _commissionAdress, _commission);
+    } 
+}
